204a0cd73159a4b1a5c85341312c61b66b041bd1
yeoni sugar <rkdgkdus522@naver.com>
2022-03-10 02:00:09 +0900

    Front 개발 환경 초기 세팅
    * React - Typescript
    * prettier
    * eslint
    
    - 작성자 : 강하연
    - 작성일 : 2022-03-10

--- Front/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Front CI/CD Test!!!</div>;
-}
-
-export default App;
---
+++ Front/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return <div>Front dev-environment setting!</div>;
+};
+
+export default App;
